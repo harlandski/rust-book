88172d1d414250e076d5bd156c6dabdd0b4fe609
harlandski <harlandski@gmail.com>
2023-07-16 22:36:40 +0600

    Start from 3.5 Control Flow / Repetition with Loops

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,17 +1,31 @@
 fn main() {
-    let number = 7;
+    let number = 9;
 
     // Note you can't just say "if number", as number is an integer, not a boolean.
     // So "truthiness" doesn't work in the same way as javascript
     if number < 5 {
-        println!("condition was true");
+        println!("{number} is less than five");
     } else {
-        println!("condition was false");
+        println!("{number} is not less than five");
     }
-
-    let number = 3;
 
     if number !=0 {
-        println!("number was something other than zero");
+        println!("{number} is something other than zero");
     }
+
+    if number %4 == 0 {
+        println!("{number} is divisible by 4");
+    } else if {number} % 3 == 0 {
+        println!("{number} is divisible by 3");
+    } else if {number} % 2 == 0 {
+        println!("{number} is divisible by 2");
+    } else {
+        println!("{number} is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition {5} else {6};
+
+    println!("The value of number is: {number}");
+
 }
