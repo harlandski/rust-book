1285ad058f05ad8e1e9719edcbf7f82f7e76d4da
harlandski <harlandski@gmail.com>
2023-07-20 17:23:07 +0600

    Finished 3.5 except challenges

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -9,22 +9,22 @@ fn main() {
         println!("{number} is not less than five");
     }
 
-    if number !=0 {
+    if number != 0 {
         println!("{number} is something other than zero");
     }
 
-    if number %4 == 0 {
+    if number % 4 == 0 {
         println!("{number} is divisible by 4");
-    } else if {number} % 3 == 0 {
+    } else if { number } % 3 == 0 {
         println!("{number} is divisible by 3");
-    } else if {number} % 2 == 0 {
+    } else if { number } % 2 == 0 {
         println!("{number} is divisible by 2");
     } else {
         println!("{number} is not divisible by 4, 3, or 2");
     }
 
     let condition = true;
-    let number = if condition {5} else {6};
+    let number = if condition { 5 } else { 6 };
 
     println!("The value of number is: {number}");
 
@@ -36,7 +36,7 @@ fn main() {
     let mut counter = 0;
 
     let result = loop {
-        counter +=1;
+        counter += 1;
 
         if counter == 10 {
             break counter * 2;
@@ -45,4 +45,62 @@ fn main() {
 
     println!("the result is {result}.");
 
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index += 1;
+    }
+
+    // The following is simpler, and less error-prone:
+    // Note this is a JavaScript "false friend"
+    // for ... in in JavaScript gives the index
+    // Rust for ... in is like JavaScript for ... of
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    // Note that the last item of range is exclusive
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+
+    let a = [5; 10];
+    let mut sum = 0;
+    for x in a {
+        println!("x: {x}");
+        sum += x;
+    }
+    println!("{sum}");
 }
